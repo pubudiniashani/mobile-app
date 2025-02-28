41815054925fd7298b8843ca58d799c9fe5aacae
pubudiniashani <pubudiniashani2000@gmail.com>
2025-02-28 12:30:38 +0530

    layout create

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,7 +4,7 @@ export default function RootLayout() {
     return (
         <Stack>
             <Stack.Screen name='index' options={{headerShown: false}} />
-            <Stack.Screen name='dashboard' options={{headerShown: false}}/>
+            <Stack.Screen name='homepage' options={{headerShown: false}}/>
         </Stack>
     );
 }
